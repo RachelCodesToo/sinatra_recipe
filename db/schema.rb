4955d1f30a09ae5191ef0fddd4f6454e9a316202
RachelCodesToo <a.r.fausto@gmail.com>
2020-08-21 15:01:29 -0400

    rolledback users table updated to password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -21,10 +21,9 @@ ActiveRecord::Schema.define(version: 2020_08_21_001851) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.string "username"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
