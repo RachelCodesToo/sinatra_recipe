1b5f4187bbf91543712ba977373993a36656c3ea
RachelCodesToo <a.r.fausto@gmail.com>
2020-09-08 18:20:40 -0400

    added sessions, secrets, authentiticate logins

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,21 @@ class UsersController < ApplicationController
       erb :'users/show'
     end 
 
+    get '/login' do 
+        erb :'users/login'
+    end 
+
+    post '/login' do 
+        @user = User.find_by(username: params[:username])
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            erb :index
+        else 
+            redirect '/login'
+        end 
+
+    end 
+
 
 
 end 
